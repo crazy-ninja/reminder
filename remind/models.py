03ecb0f103fffeeb709ec1b0fa16caa9a8efe9f0
gup_komal <komal@flatpi.com>
2016-07-25 20:07:08 +0530

    REST API to save the data

--- remind/models.py
+++ remind/models.py
@@ -3,12 +3,11 @@ from django.db import models
 
 # Create your models here.
 class RemindMe(models.Model):
-    email = models.CharField(max_length=156, null=True, blank=True)
+    email = models.CharField(max_length=150, null=True, blank=True)
     mobile = models.CharField(max_length=10, null=True, blank=True)
-    message = models.TextField(null=True, blank=True)
-    date = models.DateField(null=True, blank=True)
-    time = models.TimeField(null=True, blank=True)
+    message = models.CharField(max_length=1000, null=True, blank=True)
+    date_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         var = self.email if self.email else self.mobile
-        return var + ', ' + str(self.date) + ', ' + str(self.time)
+        return var
--- remind/views.py
+++ remind/views.py
@@ -1,25 +1,17 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from remind.serializers import RemindMeSerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def remind_me(request):
-    if request.method == 'GET':
-        email = request.GET.get('email', None)
-        mobile = request.GET.get('mobile', None)
-        message = request.GET.get('message', None)
-        date = request.GET.get('date', None)
-        time = request.GET.get('time', None)
-        # if org_id:
-        #     try:
-        #         org = Organisation.objects.get(pk=org_id)
-        #     except(Organisation.DoesNotExist, ValueError, Organisation.MultipleObjectsReturned):
-        #         return HttpResponse('{No Data Found, Please Enter Valid Org ID}')
-        #     org_team = org.org_team.all().exclude(parent_team__isnull=False)
-        #     return render(request, template_name='org_temp.html', context={'org': org, 'org_team': org_team})
-        # else:
-        #     org = Organisation.objects.all()
-        #     serializer = OrganisationSerializer(org, many=True)
-        #     return Response(serializer.data)
+    if not request.data.get('mobile') and not request.data.get('email'):
+        return Response('Please enter either email or mobile', status=400)
+    serializer = RemindMeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
