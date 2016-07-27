af3433c05c63250013dadbaf7716d3d11c372146
crazy-ninja <mailtokgupta@gmail.com>
2016-07-27 23:19:42 +0530

    Update urls.py

--- remind/urls.py
+++ remind/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'remind.views',
     url(r'^api/remind-me/$', 'remind_me', name='remind-me'),
-    # url(r'^api/user/$', 'user_list', name='user'),
 )
